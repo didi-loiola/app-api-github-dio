a00f8f218f0c9ee84dfc55969af3d15a925eb43f
Diego Loiola <diego.alves_1996@hotmail.com>
2021-10-26 09:02:17 -0300

    Mocando resultados na tela

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import Layout from "./components/layout";
 
 function App() {
   return (
-    <div className="App">Github</div>
-      
+    <main>
+      <Layout>
+        <div>
+         <img source="https://avatars.githubusercontent.com/u/72869745?v=4" alt="Imagem de perfil"/>
+         <h1>Diego Loiola</h1>
+         <h3>Username: </h3>
+         <span>didi-loiola</span>
+         <div>
+          <h4>Followers</h4>
+          <span>5</span>
+         </div>
+         <div>
+          <h4>Starreds</h4>
+          <span>2</span>
+         </div>
+         <div>
+          <h4>Following</h4>
+          <span>5</span>
+         </div>
+        </div>
+        <div>Repositories</div>
+        <div>Starreds</div>
+      </Layout>
+    </main>
   );
 }
 
