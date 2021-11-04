381172c7cbed1290ba2ae5a25f808034faa66b77
Diego Loiola <diego.alves_1996@hotmail.com>
2021-11-04 09:09:53 -0300

    Adionando novo componente NoSearch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Layout from "./components/layout";
+import NoSearch from "./components/no-search";
 import Profile from "./components/profile";
 import Repositories from "./components/repositories";
 import useGithub from "./hooks/github-hooks";
@@ -14,7 +15,7 @@ function App() {
                 <Repositories />
               </>
             )}
-          </>) : (<div>Nenhum usuário pesquisado</div>)}
+          </>) : <NoSearch />}
           
         </Layout>
   );
---
+++ src/components/no-search/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    margin: 16px;
+    display: flex;
+    justify-content: center;
+
+    h1 {
+        font-weight: bold;
+        font-sizer: 32px;
+        color: #0288D1;
+    }
+`
