caf49b57efc0ee5008927e344c411cca0cee3cfd
Diego Loiola <diego.alves_1996@hotmail.com>
2021-11-05 09:55:58 -0300

    Estilizando repositories e trazendo os dados usando githubState

--- src/components/repositories/index.js
+++ src/components/repositories/index.js
@@ -1,26 +1,63 @@
+import { useEffect, useState } from "react";
+import useGithub from "../../hooks/github-hooks"
 import RepositoryItem from "../repository-item"
 import * as S from "./styled"
 
-function Repositories () {
-    return <S.WrapperTabs selectedTabClassName="is-selected" selectedTabPanelClassName="is-selected">
+const Repositories = () => {
+    const { githubState, getUserRepos, getUserStarred } = useGithub();
+    const [hasUserForSearchrepos, setHasUserForSearchrepos] = useState(false);
+  
+    useEffect(() => {
+      if (githubState.user.login) {
+        getUserRepos(githubState.user.login);
+        getUserStarred(githubState.user.login);
+      }
+      setHasUserForSearchrepos(githubState.repositories);
+  
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [githubState.user.login]);
+  
+    return (
+      <>
+        {hasUserForSearchrepos ? (
+          <S.WrapperTabs
+            selectedTabClassName="is-selected"
+            selectedTabPanelClassName="is-selected"
+          >
             <S.WrapperTabList>
-                <S.WrapperTab>Repositories</S.WrapperTab>
-                <S.WrapperTab>Starred</S.WrapperTab>
+              <S.WrapperTab>Repositories</S.WrapperTab>
+              <S.WrapperTab>Starred</S.WrapperTab>
             </S.WrapperTabList>
             <S.WrapperTabPanel>
-                <RepositoryItem 
-                name="repo 1"
-                fullName="didi-loiola/contadorRegressivo"
-                linkDoRepo="https://github.com/didi-loiola/contadorRegressivo"/>
+              <S.WrapperList>
+                {githubState.repositories.map((item) => (
+                  <RepositoryItem
+                    key={item.id}
+                    name={item.name}
+                    linkDoRepo={item.full_name}
+                    fullName={item.full_name}
+                  />
+                ))}
+              </S.WrapperList>
             </S.WrapperTabPanel>
             <S.WrapperTabPanel>
-                <RepositoryItem 
-                name="repo 2"
-                fullName="didi-loiola/app-curriculo-dio"
-                linkDoRepo="https://github.com/didi-loiola/app-curriculo-dio"
-                />
+              <S.WrapperList>
+                {githubState.starred.map((item) => (
+                  <RepositoryItem
+                    key={item.id}
+                    name={item.name}
+                    linkDoRepo={item.html_url}
+                    fullName={item.full_name}
+                  />
+                ))}
+              </S.WrapperList>
             </S.WrapperTabPanel>
-        </S.WrapperTabs>
-}
+          </S.WrapperTabs>
+        ) : (
+          <></>
+        )}
+      </>
+    );
+  };
 
 export default Repositories
